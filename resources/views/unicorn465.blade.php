b9b768a518089b79bb4b50b8bafc3afd2196840c
xaaphrodite <xaaphrodite@gmail.com>
2022-01-04 21:30:23 +0700

    Initial Commit

---
+++ resources/views/unicorn465.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{ asset('assets/css/unicorn465.css') }}">
+    <title>unicorn465 | hidden service</title>
+</head>
+
+<body>
+    <div class="unicorn_465">
+        <img src="{{ asset('assets/img/unicorn465-bg.png') }}" alt="unicorn465">
+        <h1>unicorn465</h1>
+        <h4>by silentclouds.rest</h4>
+    </div>
+</body>
+
+</html>
